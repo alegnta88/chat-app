ef3da0e4c2f8ab0627cd3115ab31be0904e4b264
alegnta88 <alegnta88@gmail.com>
2025-12-18 10:24:28 +0300

    add middleware

---
+++ middleware/protectedRoute.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+export const protectedRoute = async (req, res, next) => {
+  const token = req.cookies.token || '';
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select('-password');
+    if (!user) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
